b7088e5b1b7466d37023ddb30b357a374bad57a3
AgustinAlfieri <agustinalfieri98@gmail.com>
2025-05-01 20:29:57 -0300

    Clase api03 - DSW BE - API express - get one
    Tuve que cambiar dependencias de express para que funcione

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,14 @@ app.get('/api/characters', (req, res) => {
   res.json(characters);
 });
 
+app.get('/api/characters/:id', (req, res) => {
+  const character = characters.find((c) => c.id === req.params.id);
+  /*   if (!character) {
+    return res.status(404).json({ error: 'Character not found' });
+  } */
+  res.json(character);
+});
+
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
