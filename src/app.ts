e2602c59009c76ce57d3d718a0c8ea1aab41c269
AgustinAlfieri <agustinalfieri98@gmail.com>
2025-05-02 17:04:16 -0300

    Clase api04 - DSW BE - API express - post
    Fin clase.

--- src/app.ts
+++ src/app.ts
@@ -39,14 +39,24 @@ app.get('/api/characters', (req, res) => {
 
 app.get('/api/characters/:id', (req, res) => {
   const character = characters.find((c) => c.id === req.params.id);
-  /*   if (!character) {
-    return res.status(404).json({ error: 'Character not found' });
-  } */
   res.json(character);
 });
 
 app.post('/api/characters', (req, res) => {
-  //req.body
+  const { name, characterClass, level, hp, mana, attack, items } = req.body;
+  const id = crypto.randomUUID();
+  const character = new Character(
+    id,
+    name,
+    characterClass,
+    level,
+    hp,
+    mana,
+    attack,
+    items
+  );
+  characters.push(character);
+  res.status(201).send({ message: 'Character created', data: character });
 });
 
 app.listen(3000, () => {
